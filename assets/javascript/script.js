dfe040cc92de8dfb73597657618cfc16b91c7655
Jaime <jaimebmorin@gmail.com>
2017-12-19 19:34:50 -0600

    JS changes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,17 @@
 //javascript for macro manager
 
+
+
+
 $(document).ready(function() {
 
 
-    var URL = "https://api.edamam.com/search%";
-    var apiKey = "app_key=bb6760c1841801cc559cf31d43145900%";
-    var appID = "app_id=6a2a24ba%";
+    var URL = "https://api.edamam.com/search?";
+    var apiKey = "app_key=bb6760c1841801cc559cf31d43145900&";
+    var appID = "app_id=6a2a24ba&";
     var q = "q=chicken";
     var queryURL = URL+apiKey+appID+q;
+    console.log(queryURL);
 
     $.ajax({
         url: queryURL,
