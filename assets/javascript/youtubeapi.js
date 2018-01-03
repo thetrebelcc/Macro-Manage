7123933f7a6d0f9c1dd983ea15a7532fa46a89cc
Fabian Anguiano <fabiananguiano@gmail.com>
2018-01-02 19:37:29 -0600

    none

---
+++ assets/javascript/youtubeapi.js
@@ -0,0 +1,15 @@
+//youtuube api 
+
+$(document).ready(function)(){
+
+	var URL= ""
+	var api key= "AIzaSyB3HrudHa8d2ZNwiZthiEzjMWlT1YI95wY"
+
+
+}
+
+console.log(queryURL);
+$.ajax({
+	url: queryURL
+	method: "GET"
+})
