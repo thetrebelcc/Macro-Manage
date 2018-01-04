9d3c7f9282dca5c56f2c2135ecd9214a8e1f4c03
jjrivasjr <jjrivasjr1@gmail.com>
2018-01-03 21:07:02 -0600

    nutrition

--- Java/logic.js
+++ Java/logic.js
@@ -1,3 +1,28 @@
+function displayRecipies(){
+  var URL = "https://api.edamam.com/search?";
+    var apiKey = "app_key=995949774bfd326ffc014061f6b2e523&";
+    var appID = "app_id=11c5a69a&";
+    var q = "q=chicken&";
+    var carb = "nutrients[FIBTG]=30-50";
+    var queryURL = URL+apiKey+appID+q+carb;
+
+    console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).done(function(response) {
+        console.log(response);
+        for (var i = 0; i<response.hits.length; i++) {
+        var carbAmt = response.hits[i].recipe.totalNutrients.FIBTG.quantity;
+        console.log(carbAmt);
+
+        }
+
+      });
+}
+
+
+
 //intialze firebase
 
 
@@ -49,6 +74,8 @@ var proteins = $("#protein-input").val().trim();
 });
 
 });
+
+$(document).on("click", ".button-1", displayRecipies);
 
 
  // database.on("value")
